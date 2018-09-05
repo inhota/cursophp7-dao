c498c9179d07857c4fb21b16dbe704bf11b68a35
Inhota <julioeng2012@gmail.com>
2018-09-05 19:03:42 -0300

    Iniciamos o Projeto

--- class/Sql.php
+++ class/Sql.php
@@ -6,7 +6,7 @@ class Sql extends PDO
 
 	public function __construct()
 	{
-		$this->conn = new  PDO("mysql:host=localhost;dbname=dbphp7","root", "");
+		$this->conn = new  PDO("mysql:host=localhost;dbname=db_php7","root", "");
 	}
 
 	private function setParams($statment, $parameters = array())
